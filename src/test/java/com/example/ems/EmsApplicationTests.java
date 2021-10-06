64277b861db82a656627c2974e3d9766a943847f
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-10-06 20:16:35 +0530

    Test

--- src/test/java/com/example/ems/EmsApplicationTests.java
+++ src/test/java/com/example/ems/EmsApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.ems;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
